287ed6639343f98f7d43994a72c786d0b88a9271
Laura <laura.coombs07@gmail.com>
2018-11-05 12:16:41 -0700

    update

---
+++ two-fer.js
@@ -0,0 +1,11 @@
+export const twoFer = (name) => {
+    if (name === 'Alice') {
+        return 'One for Alice, one for me.';
+    }
+    else if (name === 'Bob') {
+        return 'One for Bob, one for me.';
+    }
+    else {
+        return 'One for you, one for me.';
+    }
+}
